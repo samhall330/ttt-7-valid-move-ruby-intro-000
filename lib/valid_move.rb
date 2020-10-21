f63f361bee294da21cd130e6f842911f45dd1cf5
Sam Hall <mann.samantha11@gmail.com>
2020-10-21 19:56:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,15 @@ def valid_move?(board, index)
 
   if position_taken?(board, index) == true
     false
-
+  
   else board[index] == index.between?(0,8) && position_taken?(board, index) == false
     true
-
   end
+
+  if board[index]>8
+    false
+  end
+    
 end
 
 
